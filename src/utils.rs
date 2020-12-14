54e092a8c9cc62a4ee7b8226c86b93098695b5d2
alienzj <alienchuj@gmail.com>
2020-12-14 16:26:24 +0800

    add compression wrapper

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,43 @@
+// https://github.com/natir/fpa/blob/master/src/file.rs
+use std::io;
+use std::io::{BufReader, BufWriter};
+
+pub fn get_input(input_name: &str) -> (Box<dyn io::Read>, niffler::compression::Format) {
+    match input_name {
+        "-" => niffler::get_reader(Box::new(BufReader::new(io::stdin())))
+            .expect("File is probably empty"),
+        _ => niffler::from_path(input_name).expect("File is probably empty"),
+    }
+}
+
+pub fn choose_compression(
+    input_compression: niffler::compression::Format,
+    compression_set: bool,
+    compression_value: &str,
+) -> niffler::compression::Format {
+    if !compression_set {
+        return input_compression;
+    }
+    match compression_value {
+        "gzip" => niffler::compression::Format::Gzip,
+        "bzip2" => niffler::compression::Format::Bzip,
+        "lzma" => niffler::compression::Format::Lzma,
+        _ => niffler::compression::Format::No,
+    }
+}
+
+pub fn get_output(output_name: &str, format: niffler::compression::Format) -> Box<dyn io::Write> {
+    match output_name {
+        "-" => niffler::get_writer(
+            Box::new(BufWriter::new(io::stdout())),
+            format,
+            niffler::compression::Level::One,
+        )
+        .unwrap(),
+        _ => niffler::to_path(output_name, format, niffler::compression::Level::One).unwrap(),
+    }
+}
+
 // reference
 // https://github.com/ekimb/rust-mdbg/blob/master/src/utils.rs
 
